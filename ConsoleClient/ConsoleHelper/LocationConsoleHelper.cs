1cbd8e801f3e79a3ac9f7dd3d09f8610b9bfc98a
oskar <ooskar@gmx.de>
2023-01-20 12:50:59 +0100

    Adding new way of parsing input strings to location objects

--- ConsoleClient/ConsoleHelper/LocationConsoleHelper.cs
+++ ConsoleClient/ConsoleHelper/LocationConsoleHelper.cs
@@ -1,4 +1,5 @@
-﻿using Lib.Constants;
+﻿using ConsoleClient.Extensions;
+using Lib.Constants;
 using Lib.Extensions;
 using Lib.GameEntities;
 
@@ -8,53 +9,16 @@ namespace ConsoleClient.ConsoleHelper
     {
         public static Location AskForLocation()
         {
-            return new Location()
+            Console.Write("location: ");
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
             {
-                X = AskForX(),
-                Y = AskForY(),
-            };
-        }
-
-        public static char AskForX()
-        {
-            Console.Write($"X ({GameFieldConstants.XBordersAsString}): ");
-
-            string? message = Console.ReadLine();
-
-            if (String.IsNullOrWhiteSpace(message))
-            {
-                Console.WriteLine("Invalid Input!");
-                return AskForX();
-            }
-            char x = message.ToCharArray()[0];
-
-            if (x.AsInt() < 1 || x.AsInt() > GameFieldConstants.Size)
-            {
-                Console.WriteLine("X is not in the correct range!");
-                return AskForX();
-            }
-            return x;
-        }
-
-        public static int AskForY()
-        {
-            Console.Write($"Y ({GameFieldConstants.YBordersAsString}): ");
-
-            bool success = int.TryParse(Console.ReadLine(), out int y);
-
-            if (!success)
-            {
-                Console.WriteLine("Failed to parse y!");
-                return AskForY();
+                Console.WriteLine("Invalid input!");
+                return AskForLocation();
             }
 
-            if (y < 1 || y > GameFieldConstants.Size)
-            {
-                Console.WriteLine("Y is not in the correct range!");
-                return AskForY();
-            }
-
-            return y;
+            return input.ParseLocation();
         }
     }
 }
---
+++ ConsoleClient/Extensions/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Lib.GameEntities;
+
+namespace ConsoleClient.Extensions
+{
+	public static class StringExtentsion
+	{
+		public static Location ParseLocation(this string value)
+		{
+			char[] chars = value.Trim().ToCharArray();
+
+			string y = "";
+			char? x = null;
+
+			foreach (char c in chars)
+			{
+				if (!char.IsNumber(c))
+				{
+					x = c;
+				}
+				else
+				{
+					y += Convert.ToInt32(c.ToString());
+				}
+			}
+
+			if (x == null || string.IsNullOrEmpty(y))
+			{
+				return new Location();
+			}
+
+			return new Location() { X = (char) x, Y = Convert.ToInt32(y) };
+		}
+	}
+}
+
